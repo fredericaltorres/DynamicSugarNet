097b96d30b37a3769e13b2a2d66ce4349587d2b5
frederic.torres@bigtincan.com <frederic.torres@bigtincan.com>
2024-03-21 15:43:34 -0400

    Update...

---
+++ DynamicSugarStandard/Processor/DSSharp.Processor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text.RegularExpressions;
+#if !MONOTOUCH
+using System.Dynamic;
+#endif
+
+namespace DynamicSugar
+{
+    /// <summary>
+    /// Dynamic Sharp Exception
+    /// </summary>
+    public class ProcessorFailedException : System.Exception {
+
+        public ProcessorFailedException(string message) : base(message) { }
+    }
+    /// <summary>
+    /// Dynamic Sharp Helper Class
+    /// </summary>
+    public static partial class DS
+    {
+        public class Processor
+        {
+            public string SourceText { get; }
+            public string TextToBeProcessed { get; set; }
+
+            public Dictionary<string, string> Macros = new Dictionary<string, string>();    
+
+            public Processor(string text)
+            {
+                SourceText = text;
+            }
+
+            public Dictionary<string, string> ExtractMacros()
+            {
+                var linesWithNoDashDefiles = new List<string>();
+
+                var lines = SourceText.SplitByCRLF();
+                foreach (var line in lines)
+                {
+                    if (line.TrimStart().StartsWith("#"))
+                    {
+                        var m = Regex.Match(line, @"#define\s+(\w+)\s+(.*)");
+                        if (m.Success)
+                        {
+                            var name = m.Groups[1].Value;
+                            var value = m.Groups[2].Value;
+
+                            if (!string.IsNullOrEmpty(value))
+                            {
+                                value = value.Trim();
+                                if(value.StartsWith("\"") && value.EndsWith("\""))
+                                    value = value.Substring(1, value.Length - 2);
+                            }
+                            Macros.Add(name, value);
+                        }
+                        linesWithNoDashDefiles.Add("");
+                    }
+                    else linesWithNoDashDefiles.Add(line);
+                }
+                TextToBeProcessed = string.Join(Environment.NewLine, linesWithNoDashDefiles) + Environment.NewLine;
+                return Macros;
+            }
+
+            public string Process()
+            {
+                var text = TextToBeProcessed;
+                foreach (var m in Macros)
+                    text = text.Replace(m.Key, m.Value);
+
+                return text;
+            }
+        }
+    }
+}
---
+++ UnitTests/Processor/DS.Processor.UnitTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicSugar;
+using System.Dynamic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace DynamicSugarSharp_UnitTests
+{
+    [TestClass]
+    public class DS_Processor
+    {
+        const string SOURCE_TEXT1 = @"
+#define __FOO__ foo
+#define __BAR__ bar
+
+message1: __FOO__ 
+message2: __BAR__
+";
+
+        [TestMethod]
+        public void Processor_Process_Basic()
+        {
+            var p = new DS.Processor(SOURCE_TEXT1);
+            var macros = p.ExtractMacros();
+            Assert.AreEqual(2, macros.Count);
+
+            var expected = @"
+
+message1: foo 
+message2: bar
+";
+
+            var result = p.Process();
+            Assert.AreEqual(expected, result);
+        }
+
+
+        const string SOURCE_TEXT2_STRING = @"
+#define __FOO__ ""foo""
+#define __BAR__ ""bar""
+
+message1: __FOO__ 
+message2: __BAR__
+";
+
+        [TestMethod]
+        public void Processor_Process_Strings()
+        {
+            var p = new DS.Processor(SOURCE_TEXT2_STRING);
+            var macros = p.ExtractMacros();
+            Assert.AreEqual(2, macros.Count);
+
+            var expected = @"
+
+message1: foo 
+message2: bar
+";
+
+            var result = p.Process();
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+
+
+
+}
