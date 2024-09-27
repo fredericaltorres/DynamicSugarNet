52466e3c8601c6cb3b9b59c6ed522352eca33807
FredericALTorres <frederic.torres@bigtincan.com>
2024-09-27 16:46:11 -0400

    Update...

---
+++ DynamicSugarStandard/TextFileHelper/TextFileReWriter.cs
@@ -0,0 +1,82 @@
+﻿using DynamicSugar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DynamicSugar.TextFileHelper
+{
+    public static class TextFileReWriter
+    {
+        const string LOG_TO_MARK = "#logto:";
+
+        public static string ProcessLogToMacro(string text, string splitString)
+        {
+            var lines = text.Split(DS.List(splitString).ToArray(), StringSplitOptions.None).ToList();
+            var newLines = new List<string>();
+
+            foreach (var line0 in lines)
+            {
+                var line1 = line0;
+                if (line1.Contains(LOG_TO_MARK))
+                {
+                    var logToFileName = line1.Substring(line1.IndexOf(LOG_TO_MARK) + LOG_TO_MARK.Length);
+                    var logToText = line1.Substring(0, line1.IndexOf(LOG_TO_MARK) - 1);
+
+                    File.AppendAllText(logToFileName, logToText + Environment.NewLine);
+                }
+                newLines.Add(line1);
+            }
+            return text;
+        }
+
+        public static void Rewrite(string fileName, string splitString, Func<string, List<string>> fn)
+        {
+            var lines = ReadLines(fileName, splitString);
+            var newLines = new List<string>();
+
+            lines.ForEach((line) => newLines.AddRange(fn(line)));
+
+            WriteFile(fileName, splitString, newLines);
+        }
+
+        public static void Insert(string fileName, string splitString, string line, int lineIndex)
+        {
+            var lines = ReadLines(fileName, splitString);
+
+            lines.Insert(lineIndex, line);
+
+            WriteFile(fileName, splitString, lines);
+        }
+
+        public static void Add(string fileName, string splitString, string newLine)
+        {
+            var lines = ReadLines(fileName, splitString);
+
+            lines.Add(newLine);
+
+            WriteFile(fileName, splitString, lines);
+        }
+
+        private static List<string> ReadLines(string fileName, string splitString)
+        {
+            var text = File.ReadAllText(fileName);
+            var lines = text.Split(DS.List(splitString).ToArray(), StringSplitOptions.None).ToList();
+            return lines;
+        }
+
+        public static void WriteFile(string fileName, string splitString, List<string> newLines)
+        {
+            if(File.Exists(fileName))
+                File.Delete(fileName);
+            File.WriteAllText(fileName, string.Join(splitString, newLines));
+        }
+
+        public static void WriteFile(string fileName, string text)
+        {
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+            File.WriteAllText(fileName, text);
+        }
+    }
+}
---
+++ UnitTests/TextFileHelper/TextFileHelper_UnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicSugar;
+using System.Dynamic;
+using System.Reflection;
+using System.Web.UI.WebControls;
+using DynamicSugar.TextFileHelper;
+
+namespace DynamicSugarSharp_UnitTests {
+
+    [TestClass]
+    public class TextFileHelper_UnitTests
+    {
+        const string LogToTextSourceSample = @"
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Video Processing Performance: 5 in process at  2024/09/27 04:33:48.087 PM #logto:c:\temp\ProcessLogToMacro.log
+Tutu Errors:
+Video Processing Performance: 9 in process at  2024/09/27 05:55:48.087 PM #logto:c:\temp\ProcessLogToMacro.log
+";
+
+        [TestMethod]
+        public void ProcessLogToMacro()
+        {
+            var tfh = new TestFileHelper();
+            var logToFileName = @"c:\temp\ProcessLogToMacro.log";
+            tfh.DeleteFile(logToFileName);
+            
+            var textResult = TextFileReWriter.ProcessLogToMacro(LogToTextSourceSample, Environment.NewLine);
+
+            Assert.IsTrue(File.Exists(logToFileName));
+            Assert.IsTrue(File.ReadAllText(logToFileName).Contains("5 in process"));
+            Assert.IsTrue(File.ReadAllText(logToFileName).Contains("9 in process"));
+            Assert.AreEqual(LogToTextSourceSample, textResult);
+
+            tfh.DeleteFile(logToFileName);
+        }
+    }
+}
