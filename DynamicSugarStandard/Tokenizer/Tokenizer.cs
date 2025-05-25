0079a6f001b390085c21526fbbf795da2d508c2c
FredericALTorres <frederic.torres@bigtincan.com>
2025-05-24 23:44:10 -0400

    Update...

---
+++ DynamicSugarStandard/Tokenizer/Tokenizer.cs
@@ -0,0 +1,213 @@
+﻿using DynamicSugar;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicSugar
+{
+    public class Tokenizer
+    {
+        /*
+           Create a method that tokenizes a string into a list of tokens.
+           the characters that are used to tokenize are:
+           - " " (space)
+           - "," (comma)
+           - "." (dot)
+           - ";" (semicolon)
+           - "(" (open parenthesis)
+           - ")" (close parenthesis)
+           - "[" (open bracket)
+           - "]" (close bracket)
+           - "{" (open brace)
+           - "}" (close brace)
+           - "=" (equal)
+           - "!" (exclamation mark)
+           - ">" (greater than)
+           - "<" (less than)
+           - "&" (ampersand)
+           - "|" (pipe)
+           - "^" (caret)
+           - "~" (tilde)
+           - "`" (backtick)
+           - "'" (single quote)
+           - "/" (slash)
+           - "\\" (backslash)
+           - "+" (plus)
+           - "-" (minus)
+           - "*" (asterisk)
+           - "%" (percent)
+           - "@" (at)
+           - "#" (hash)
+           - "$" (dollar)
+           - " " (space)
+
+
+           - "\"" (double quote string should be tokenized as a single token)
+        */
+
+        public enum TokenType
+        {
+            Identifier,
+            StringLiteral,
+            Number,
+            Operator,
+            Delimiter,
+            Keyword,
+            Comment,
+
+
+            UndefinedToken,
+            DateToken,
+            DateTimeToken,
+        }
+
+        public class Token {
+
+            public string Value { get; set; }
+            public TokenType Type { get; set; }
+
+            public Token(string value, TokenType type)
+            {
+                Value = value;
+                Type = type;
+            }
+
+            public static Token GetUndefinedToken()
+            {
+                return new Token(null, TokenType.UndefinedToken);
+            }
+            
+
+            public bool IsIdentifier => Type == TokenType.Identifier;
+            public bool IsNumber => Type == TokenType.Number;
+            public bool IsDelimiter => Type == TokenType.Delimiter;
+        }
+
+        public List<Token> Tokenize(string input)
+        {
+            var tokens = new List<Token>();
+
+            int i = 0;
+            while (i < input.Length)
+            {
+                // Skip whitespace
+                if (char.IsWhiteSpace(input[i]))
+                {
+                    i++;
+                    continue;
+                }
+
+                // Handle string literals
+                if (input[i] == '"')
+                {
+                    var stringBuilder = new StringBuilder();
+                    i++; // Skip opening quote
+                    while (i < input.Length && input[i] != '"')
+                    {
+                        stringBuilder.Append(input[i]);
+                        i++;
+                    }
+                    if (i < input.Length) i++; // Skip closing quote
+                    tokens.Add(new Token(stringBuilder.ToString(), TokenType.StringLiteral));
+                    continue;
+                }
+
+                // Handle numbers
+                if (char.IsDigit(input[i]))
+                {
+                    var stringBuilder = new StringBuilder();
+                    while (i < input.Length && (char.IsDigit(input[i]) || input[i] == '.'))
+                    {
+                        stringBuilder.Append(input[i]);
+                        i++;
+                    }
+                    tokens.Add(new Token(stringBuilder.ToString(), TokenType.Number));
+                    continue;
+                }
+
+                // Handle delimiters and operators
+                if (IsDelimiter(input[i]))
+                {
+                    tokens.Add(new Token(input[i].ToString(), TokenType.Delimiter));
+                    i++;
+                    continue;
+                }
+
+                // Handle identifiers and keywords
+                var identifierBuilder = new StringBuilder();
+                while (i < input.Length && !IsDelimiter(input[i]) && !char.IsWhiteSpace(input[i]))
+                {
+                    identifierBuilder.Append(input[i]);
+                    i++;
+                }
+                if (identifierBuilder.Length > 0)
+                {
+                    tokens.Add(new Token(identifierBuilder.ToString(), TokenType.Identifier));
+                }
+            }
+            
+            return CombineTokens(tokens);
+        }
+
+        public Token GetToken(List<Token> tokens, int x, int inc = 0)
+        {
+            if( x + inc < 0 || x + inc >= tokens.Count)
+                return Token.GetUndefinedToken();
+            return tokens[x + inc];
+        }
+
+        public Token GetPreviousToken(List<Token> tokens, int x, int dec)
+        {
+            if (x-dec < 0 || x-dec >= tokens.Count)
+                return Token.GetUndefinedToken();
+            return tokens[x - dec];
+        }
+
+        public List<Token> CombineTokens(List<Token> tokens)
+        {
+            var x = 0;
+            var r = new List<Token>();
+
+            while (x < tokens.Count)
+            {
+                if(GetToken(tokens, x).IsNumber && 
+                   GetToken(tokens, x, 1).IsDelimiter &&
+                   GetToken(tokens, x, 2).IsNumber && 
+                   GetToken(tokens, x, 3).IsDelimiter &&
+                   GetToken(tokens, x, 4).IsNumber
+                   )
+                {
+                    var dateStr = ConcatTokens(tokens, x, 5);
+                    x += 5;
+                    r.Add(new Token(dateStr, TokenType.DateToken));
+                }
+                else
+                {
+                    r.Add(GetToken(tokens, x));
+                    x++;
+                }
+            }
+
+            return r;
+        }
+
+        public string ConcatTokens(List<Token> tokens, int start, int count)
+        {
+            var sb = new StringBuilder();
+            for (int i = start; i < start + count && i < tokens.Count; i++)
+                sb.Append(tokens[i].Value);
+            return sb.ToString();
+        }
+        
+
+        private bool IsDelimiter(char c)
+        {
+            return c == ' ' || c == ',' || c == '.' || c == ';' ||
+                   c == '(' || c == ')' || c == '[' || c == ']' ||
+                   c == '{' || c == '}' || c == '=' || c == '!' ||
+
+                   c == '-' || c == ':' ||
+
+                   c == '>' || c == '<' || c == '&' || c == '|' ;
+        }
+    }
+}
---
+++ UnitTests/Tokenizer/Tokenizer_UnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicSugar;
+using System.Dynamic;
+using System.Reflection;
+
+namespace DynamicSugarSharp_UnitTests {
+
+    [TestClass]
+    public class Tokenizer_UnitTests
+    {
+        const string TestLogString1 = @"2025-05-24 A[B=2]";
+        const string TestLogString2 = @"2025-05-24 13:16:52.859,Info,Export,[id: 709046703, mode: Export][ExecuteConversion()]Slide 10755223, type: IMAGE, index: 0001";
+
+        [TestMethod]
+        public void Tokenizer()
+        {
+            var tokenizer = new Tokenizer();
+            var tokens = tokenizer.Tokenize(TestLogString1);
+
+            var x = 0;
+            Assert.AreEqual("2025-05-24", tokens[x++]);
+            Assert.AreEqual("A", tokens[x++]);
+            Assert.AreEqual("B", tokens[x++]);
+            Assert.AreEqual("2", tokens[x++]);
+        }
+    }
+}
