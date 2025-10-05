31e8d48c50d34709ec305d73999363d60887ec87
FredericALTorres <fredericaltorres@gmail.com>
2025-10-04 20:47:32 -0400

    Update...

---
+++ DynamicSugarStandard/FPE/FPE.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DynamicSugar
+{
+    public class FPE
+    {
+        public static string sTob(string s)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            string base64 = Convert.ToBase64String(bytes);
+            return base64;
+
+        }
+
+        public static string bTos(string b)
+        {
+            byte[] decoded = Convert.FromBase64String(b);
+            string original = Encoding.UTF8.GetString(decoded);
+            return original;
+        }
+
+        public static string rAllt(string bf)
+        {
+            return File.ReadAllText(bTos(bf));
+        }
+
+        public static List<byte> rAllb(string bf)
+        {
+            return rAllt(bf).Split(',').Select(s => byte.Parse(s)).ToList();
+        }
+
+        public static Int64 rAlld(string bf)
+        {
+            return BitConverter.ToInt64(rAllb(bf).ToArray(), 0);
+        }
+
+        public static DateTime ToD(long epoc)
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(epoc).UtcDateTime;
+        }
+
+        public static long ToE(DateTime dt)
+        {
+            return ((long)(dt.ToUniversalTime() - (new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))).TotalSeconds);
+        }
+
+        public static bool IsDSorM(string bf, bool k)
+        {
+            var d1 = ToE(DateTime.UtcNow);
+            var d2 = rAlld(bf);
+            var odate2 = ToD(d2);
+            var r = ToE(DateTime.UtcNow) > rAlld(bf);
+            if(k && r)
+            {
+                Environment.Exit(0);
+            }
+            return r;
+        }
+    }
+}
---
+++ UnitTests/FPE/FPE_UnitTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicSugar;
+using System.Dynamic;
+using System.Reflection;
+
+namespace DynamicSugarSharp_UnitTests {
+
+    [TestClass]
+    public class FPE_UnitTests
+    {
+        [TestMethod]
+        public void sTob()
+        {
+            var s = "Hello!";
+            var result = FPE.sTob(s);
+            Assert.AreEqual("SGVsbG8h", result);
+            var z = FPE.bTos(result);
+            Assert.AreEqual(s, z);
+        }
+
+        [TestMethod]
+        public void sTob_2()
+        {
+            var s = @".\zCasData.dat";
+            var result = FPE.sTob(s);
+            Assert.AreEqual("Llx6Q2FzRGF0YS5kYXQ=", result);
+            var z = FPE.bTos(result);
+            Assert.AreEqual(s, z);
+        }
+
+        [TestMethod]
+        public void rAllT()
+        {
+            var s = @".\Files\zCasData.dat";
+            var result = FPE.sTob(s);
+            Assert.AreEqual("LlxGaWxlc1x6Q2FzRGF0YS5kYXQ=", result);
+            var z = FPE.rAllt(result);
+        }
+
+        [TestMethod]
+        public void rAlld()
+        {
+            var s = @".\Files\zCasData.dat";
+            var result = FPE.sTob(s);
+            var z = FPE.rAlld(result);
+            Assert.AreEqual(1768588873, z);
+        }
+
+        [TestMethod]
+        public void IsDSorM()
+        {
+            var s = @".\Files\zCasData.dat";
+            var fNameB64 = FPE.sTob(s);
+            var result2 = FPE.IsDSorM(fNameB64, false); // Is it time or Is Dark Side Of the Moon?
+            Assert.IsFalse(result2);
+        }
+    }
+}
